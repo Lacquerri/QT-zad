eb05a36cab6cd0c3a551f5365da2d8e3e4a45e5c
Bartłomiej Pawlik <148866688+Lacquerri@users.noreply.github.com>
2025-01-08 23:19:45 +0100

    symulator_rozgrywek

---
+++ symulator_rozgrywek/mainwindow.cpp
@@ -0,0 +1,112 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <random>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->listWidget->addItem("Pawlik");
+    ui->listWidget->addItem("Adamczyk");
+    ui->listWidget->addItem("Jędrzejowski");
+    ui->listWidget->addItem("Kowalski");
+    ui->listWidget->addItem("Stolarski");
+    ui->listWidget->addItem("Ziółkowski");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    if(nr_etapu > 0)
+    {
+    nr_etapu--;
+    ui->label->setText(etap[nr_etapu]);
+    }
+
+    else
+    {
+        nr_etapu = 2;
+        ui->label->setText(etap[nr_etapu]);
+    }
+
+    if(!ui->lineEdit_2->text().isEmpty())
+        ui->pushButton_3->setEnabled(1);
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    if(nr_etapu < 2)
+    {
+        nr_etapu++;
+        ui->label->setText(etap[nr_etapu]);
+    }
+
+    else
+    {
+        nr_etapu = 0;
+        ui->label->setText(etap[nr_etapu]);
+    }
+
+    if(!ui->lineEdit_2->text().isEmpty())
+        ui->pushButton_3->setEnabled(1);
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    std::random_device gen_los;
+    std::mt19937 gen_plos;
+    gen_plos.seed(gen_los());
+    std::uniform_int_distribution<int> uniDistInt(1, 2);
+    if(uniDistInt(gen_plos)==1){
+        ui->lineEdit->setStyleSheet("background-color: green;");
+        ui->lineEdit_2->setStyleSheet("background-color: red;");
+        QMessageBox::information(this, "Winner", ui->lineEdit->text());
+    }
+    else if(uniDistInt(gen_plos)==2){
+        ui->lineEdit_2->setStyleSheet("background-color: green;");
+        ui->lineEdit->setStyleSheet("background-color: red;");
+        QMessageBox::information(this, "Winner", ui->lineEdit_2->text());
+    }
+
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    ui->lineEdit->clear();
+    ui->lineEdit_2->clear();
+
+    nr_etapu = -1;
+    ui->label->setText("Etap");
+    ui->pushButton_3->setEnabled(0);
+
+    ui->lineEdit_2->setStyleSheet("background-color: white;");
+    ui->lineEdit->setStyleSheet("background-color: white;");
+}
+
+
+
+
+
+void MainWindow::on_listWidget_itemDoubleClicked(QListWidgetItem *item)
+{
+    if(ui->lineEdit->text().isEmpty())
+    ui->lineEdit->setText(item->text());
+
+    else if(ui->lineEdit_2->text().isEmpty())
+    {
+        ui->lineEdit_2->setText(item->text());
+
+        if(nr_etapu!=-1)
+        ui->pushButton_3->setEnabled(1);
+    }
+}
+
